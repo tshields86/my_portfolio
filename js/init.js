d9359fd589a52c0684a1b43dbd29e85f82d1ebc8
Travis Shields <travis.shields@gmail.com>
2016-05-17 10:29:31 -0400

    add icons and css effects

--- js/init.js
+++ js/init.js
@@ -3,5 +3,31 @@
 
     $('.button-collapse').sideNav();
 
+    var animationIcon = 'animated flip';
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    $('.icon').on('mouseover', function (){
+      $(this).addClass(animationIcon).one(animationEnd, function(){
+        $(this).removeClass(animationIcon);
+      });
+    });
+    var animationName = 'animated wobble';
+    $('.phone').on('mouseover', function (){
+      $(this).addClass(animationName).one(animationEnd, function(){
+        $(this).removeClass(animationName);
+      });
+    });
+    var animationEmail = 'animated zoomOutRight';
+    $('.email').on('mouseover', function (){
+      $(this).addClass(animationEmail).one(animationEnd, function(){
+        $(this).removeClass(animationEmail);
+      });
+    });
+    var animationContact = 'animated pulse';
+    $('.contact').on('mouseover', function (){
+      $(this).addClass(animationContact).one(animationEnd, function(){
+        $(this).removeClass(animationContact);
+      });
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
