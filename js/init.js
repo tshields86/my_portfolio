ac56042e65cf3e0bb84807cb42745e97a10a69d3
Travis Shields <travis.shields@gmail.com>
2016-06-17 02:13:23 -0400

    add comp project images, more css, resume modal

--- js/init.js
+++ js/init.js
@@ -51,5 +51,16 @@
       });
     });
 
+  // modal
+    $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      // ready: function() { alert('Ready'); }, // Callback for Modal open
+      // complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+    );
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
